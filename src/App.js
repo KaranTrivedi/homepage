7f092a06e66859a9b013202f301fcdf488bec4c3
KaranTrivedi <kytrivedi94@hotmail.com>
2023-04-01 13:36:10 -0400

    Added ant design to the project!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
-  Button,
   Heading,
   // Image,
   View,
-  Card,
 } from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
+import { 
+  Card,
+  Button,
+  ConfigProvider,
+  theme
+} from 'antd';
+import 'antd/dist/reset.css';
+
+function App({ signOut })
+{
   return (
+    <ConfigProvider
+    theme={{
+      algorithm: theme.darkAlgorithm,
+    }}
+  >
     <View className="App">
       <Card>
         {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1}>We now have Auth!</Heading>
+        <h1 level={1}>We now have Auth!</h1>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+        <Button type="primary" onClick={signOut}>Sign Out</Button>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
     </View>
+      </ConfigProvider>
+
   );
 }
 
